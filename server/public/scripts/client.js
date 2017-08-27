c98742e978f00cd6e683e00d5a5c7d4d113b88f6
Chad Sundberg <chadsundberg@gmail.com>
2017-08-27 13:11:08 -0500

    line spacing

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -89,6 +89,7 @@ function addBlocksToPage() {
     // var blockNumber = i;
     // newCarouselBlock.data(blockNumber);
     newCarouselBlock.css('background-color', 'Aqua');
+
     newCarouselBlock.addClass('carouselColorBlock');
     // newCarouselBlock.attr('#' + i );
     $('#carouselBlock').append(newCarouselBlock);
